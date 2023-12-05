9e0d8fcdfae0b46520d9beb5a8412c113cf48191
forms5nv50gitMAVR <152240646+forms5nv50gitMAVR@users.noreply.github.com>
2023-12-04 20:58:51 -0600

    Update init.js

--- init.js
+++ init.js
@@ -2,11 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+    apiKey: "AIzaSyDrhNAivvs2-EZArQ60CE_n74KQkPVtHl0",
+    authDomain: "autentica5nv50-8f345.firebaseapp.com",
+    projectId: "autentica5nv50-8f345",
+    storageBucket: "autentica5nv50-8f345.appspot.com",
+    messagingSenderId: "727738415630",
+    appId: "1:727738415630:web:8866ade7656888fd36da5f"
     });
